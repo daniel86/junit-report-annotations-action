c0f36b05c92a757d61b6ba76af33ec315a960038
Daniel Beßler <danielb@cs.uni-bremen.de>
2020-07-01 15:42:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ var parseString = require('xml2js').parseStringPromise;
                                     start_column: 0,
                                     end_column: 0,
                                     annotation_level: 'failure',
-                                    title: testCase['$'].name,
-                                    message: testCase['$'].name + ": " + testCase.failure[0]['_'],
+                                    title: testsuite['$'].name + " -- " + testCase['$'].name,
+                                    message: testCase.failure[0]['_'],
                                 });
                             }
                         }
